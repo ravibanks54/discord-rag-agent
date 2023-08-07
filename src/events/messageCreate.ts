22ad6a0b4897294a5942a117739581dead4dfade
Ravi Bhankharia <ravibanks54@users.noreply.github.com>
2023-08-06 22:24:02 -0500

    Only answer questions on enabled channels. (#6)

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,12 +1,15 @@
 import promptCMSClient from "../clients/promptcms";
 import messageToExecutionId from "../persistence/messageExecutionMapping";
 import {Message} from "discord.js";
+import enabledChannels from "../persistence/enabledChannels";
 
 export default async (message: Message) => {
     if (message.author.bot) return;
+    if (await enabledChannels.is_enabled(message.guildId!, message.channelId!)) {
+        console.log("Answering question...")
+        const resultData = await promptCMSClient.execute({question: message.content});
+        const reply = await message.reply(resultData.data.response);
+        await messageToExecutionId.set(reply.id, resultData.data.execution_id);
+    }
 
-    console.log("Answering question...")
-    const resultData = await promptCMSClient.execute({question: message.content});
-    const reply = await message.reply(resultData.data.response);
-    await messageToExecutionId.set(reply.id, resultData.data.execution_id);
 }
